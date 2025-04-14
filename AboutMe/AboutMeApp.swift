b87a695f36f9b9f3fbd9e7c86daaea82242e51b4
Sienna Baird <siennatanner21@gmail.com>
2025-04-13 21:24:26 -0700

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Sienna Tanner on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
